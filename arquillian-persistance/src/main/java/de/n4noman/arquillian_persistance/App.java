ae9a205cc904c47926560f2b000fbd75e15b320b
noman <n4noman@yahoo.com>
2014-07-31 12:59:15 +0200

    no changes

---
+++ arquillian-persistance/src/main/java/de/n4noman/arquillian_persistance/App.java
@@ -0,0 +1,13 @@
+package de.n4noman.arquillian_persistance;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
